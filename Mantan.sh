ef011ee9be6361c9f55d3ffe23070308b9e81f2a
Aulia <105323573+N3WBI31@users.noreply.github.com>
2022-06-09 18:22:05 +0700

    Add files via upload

---
+++ Mantan.sh
@@ -0,0 +1,151 @@
+#!/data/data/com.termux/files/usr/bin/bash 
+ #Upps,, Saya Mencium Ada Kang Record 
+ #Kreatif Lah Tolol, susah nih gw bikinnya :v 
+ clear 
+ blue='\e[1;34m' 
+ green='\e[1;32m' 
+ purple='\e[1;35m' 
+ cyan='\e[1;36m' 
+ red='\e[1;31m' 
+ white='\e[1;37m' 
+ yellow='\e[1;33m' 
+ echo -e $green'[+]==========================================[+]' 
+ sleep 0.1; 
+ echo -e $red'[~]=========== Welcome To My TOOLS ==========[~]' 
+ sleep 0.1; 
+ echo -e $purple'[~]===== Code by Zarel Ganteng:v | RelGanz =====[~]' 
+ sleep 0.1; 
+ echo -e $green'[+]==========================================[+]' 
+ sleep 0.1; 
+ echo -e $green'# Version  : 1.0 (unfaedah)' 
+ echo -e $cyan'|=============================================|' 
+ sleep 0.1; 
+ echo -e $red'|############# Pilih Nomor Mana? #############|'; 
+ sleep 0.1; 
+ echo -e $yellow'|=============================================|' 
+ sleep 0.1; 
+ echo -e $blue'1.Balikan Lagi Ke Mantan' 
+ sleep 0.01; 
+ echo -e $blue'2.Lupakan Mantan' 
+ sleep 0.01; 
+ echo -e $blue'3.Serang Mantan' 
+ sleep 0.01; 
+ echo -e $blue'4.Cari Pengganti Mantan' 
+ sleep 0.01; 
+ echo -e $blue'0.Keluar Dari Tools' 
+ sleep 0.03; 
+ echo -e $green "" 
+ read -p"PILIH Yang Mana ?: " Next; 
+  
+ if [ $Next = 1 ] || [ $Next = satu ] 
+ then 
+ clear 
+ echo -e $white "╭─"$yellow"WELLCOME TO MY TOOLS"$cyan' ~/Masukan Nama/Nick' 
+ read -p " ╰─$ " kontol; 
+ clear 
+ echo -e $red"   Haloo...."$green" $kontol"$red" Ini adalah Saran dari Ku" 
+ echo -e $cyan"Jika ingin Balikan lagi ke mantan maka" 
+ echo -e $cyan"persiapkan alat di bawah ini" 
+ echo -e $yellow'<<<<===========>>>>' 
+ echo -e $cyan"1.Kuota internet" 
+ echo -e $cyan"2.Kopi/Roko" 
+ echo -e $cyan"3.Ketanvanan" 
+ echo -e $yellow'<<<<===========>>>>' 
+ echo -e $cyan"Percaya Diri Pasti Kamu bisa, Ga usah malu !!" 
+ echo -e $cyan"Katakan... Maukah Kamu Kembali Dengan Aku???" 
+ echo 
+ echo -e $red'['$cyan'!'$red']' $green'<<<<<<<<>>>>>>>>' $red"["$cyan'!'$red']' 
+ echo 
+ exit 
+  
+ elif [ $Next = 2 ] || [ $Next = dua ] 
+ then 
+ clear 
+ echo -e $white "╭─"$yellow"WELLCOME TO MY TOOLS"$cyan" ~/Masukan Username Anda Di Bawah!!!"$white 
+ read -p " ╰─$ " bacod 
+ clear 
+ echo 
+ echo -e $yellow"$bacod"$red"~/Ini Saranku Untuk Kamu !!!" 
+ echo -e $green"Buka Whatsapp/Fb Dan hapus" 
+ echo -e $green"pertemanan dan hapus riwayat Chat" 
+ echo -e $green"Mantan Kamu, dan tinggalkan chat" 
+ echo -e $green"Terakhir untuk Mantan kamu, dan tulis" 
+ echo -e $green"Aku Akan Melupakan Kamu, dan aku Berharap" 
+ echo -e $green"Kamu juga melupakan aku" 
+ echo -e $red'['$cyan'!'$red']' $green'<<<<<<<<>>>>>>>>' $red"["$cyan'!'$red']' 
+ echo 
+ exit 
+  
+ elif [ $Next = 3 ] || [ $Next = 3 ] 
+ then 
+ echo -e $yellow"Apakah Kamu Yakin Ingin Serang Mantan ?" 
+ read -p"Ketik Y/T ==>: " bl; 
+  
+ elif [ $Next = 4 ] || [ $Next = 4 ] 
+ then 
+ clear 
+ echo -e $white "╭─"$yellow"WELLCOME TO MY TOOLS"$cyan" ~/Masukan Nama/Nick Anda Di Bawah!!!"$white 
+ read -p " ╰─$ " kontol 
+ clear 
+ echo -e $red"   Haloo...."$green" $kontol"$red" Ini adalah Saran dari Ku untuk kamu" 
+ echo -e $green"jika kamu ingin cari pengganti, maka kamu" 
+ echo -e $green"harus sering berikhtiar dan berdoa agar kamu" 
+ echo -e $green"menadapatkan seseorang yang setia" 
+ echo -e $green"1. buka fb atau yang lainnya" 
+ echo -e $green"2. cari wanita yang kalian inginkan (saran yg berhijab" 
+ echo -e $green"3. mulai lah chat dengan wanita yang kamu inginkan" 
+ echo -e $green"4. rutin chat dia minimal 2× perhari" 
+ echo 
+ exit 
+  
+ elif [ $Next = 0 ] || [ $Next = keluar ] 
+ then 
+ echo -e $green'Mau Keluar ya ?' 
+ sleep 1.1; 
+ echo -e $blue'Ya sudah lah, gw ga butuh lo,' 
+ sleep 1; 
+ echo -e $cyan"Salam Dari saya Zarel Ganz :)" 
+ echo 
+ exit 
+ logout 
+  
+ else 
+ echo -e $cyan'Ente Kntl, tq' 
+ sleep 2; 
+ bash mantan.sh 
+ fi 
+  
+ if [ $bl = Y ] || [ $bl = y ] 
+ then 
+ clear 
+ figlet Loading|lolcat 
+ sleep 1; 
+ clear 
+ figlet Loading.|lolcat 
+ sleep 1; 
+ clear 
+ figlet Loading..|lolcat 
+ sleep 1; 
+ clear 
+ figlet Loading...|lolcat 
+ sleep 1; 
+ clear 
+ figlet Loading....|lolcat 
+ sleep 1; 
+ clear 
+ figlet Loading.....|lolcat 
+ sleep 1; 
+ clear 
+ git clone https://github.com/N3WBI31/Santet0nline 
+ ls 
+ cd Santet0nline
+ sh Santet.sh
+  
+ elif [ $bl = T ] || [ $bl = t ] 
+ then 
+ clear 
+ figlet Yasudah|lolcat 
+ echo -e $yellow"mungkin ini keputusan terbaik mu" 
+ exit 
+ fi 
+ 
